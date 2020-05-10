75ea8b7795d620bf9751c40541cadc29160775a4
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-05-10 18:21:07 +0500

    Update Error.py

--- Error.py
+++ Error.py
@@ -255,7 +255,6 @@ def pilih_super():
         jalan('\033[1;91mPlease Wait\033[1;94m...')
         titik = ['.   ','..  ','... ']
         for o in titik:
-	print("\r\033[1;95mCloning\033[1;91m"+o),;sys.stdout.flush();time.sleep(0.001)
 	print "\n\033[1;91m«--•◈••◈•---\x1b[1;95m•◈•Stop Process Press CTRL+Z•◈•\033[1;91m---•◈••◈•-»"
 	print "\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•\033[1;91mBlackMafia\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬ •◈•"
 	jalan(' \033[1;91m.................\033[1;95mCloning Start..\033[1;91m............ ')
