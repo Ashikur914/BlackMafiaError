3297437134a7b8f9e091cc5701bb8685102c156c
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-05-10 18:10:34 +0500

    Update Error.py

--- Error.py
+++ Error.py
@@ -21,17 +21,25 @@ info = time.strftime("%S:%M:%H")
 def quit():
     print '\x1b[1;91m[!] Program Closed'
     os.sys.exit()
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
 def loading(z):
     for e in z + '\n':
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(0.01)
-def jalan(z):
-	for e in z + '\n':
-		sys.stdout.write(e)
-		sys.stdout.flush()
-		time.sleep(0.01)
-
 ###########################################################################
 #                              Color                                      #
 ###########################################################################
